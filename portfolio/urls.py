da4ea93caaa9a250b43e8e5f1fea9a7803ad3ac3
unknown <bocchi.dev@gmail.com>
2021-01-02 17:19:31 -0300

    one step forward

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
-from .views import Home
+from .views import Home, Article
 
 
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('articles', Article.as_view())
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-#Create your views here.
+from datetime import datetime
+
+# Create your views here.
 
 # Function based view
 # def home(request):
@@ -10,7 +12,30 @@ from django.views import View
 #
 # Class based view
 class Home(View):
-    def get(self,request):
+
+    def get(self, request):
         return HttpResponse('Welcome to my portfolio!')
-    def post(self,request):
+
+    def post(self, request):
         return HttpResponse('What are you trying to post?')
+
+class Article(View):
+
+    def get(self, request):
+        articles = [
+            {
+                "title": "Title",
+                "category": "Test",
+                "author": "Myself",
+                "content": "Just some lorem ipsum content",
+                "creation_date": datetime.now()
+            },
+            {
+                "title": "Title2",
+                "category": "Test2",
+                "author": "Myself",
+                "content": "Just some lorem ipsum content 2",
+                "creation_date": datetime.now()
+            }
+        ]
+        return render(request,'articles.html', {"articles":articles})
